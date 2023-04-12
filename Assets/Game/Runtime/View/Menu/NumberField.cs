94ef6a842bf34dbbed14743ec2084e65ed14d7c8
Konstantine Osintcev <rojuty@gmail.com>
2023-04-12 12:42:48 +0700

    main menu changed, removed unused assets

--- Assets/Game/Runtime/View/Menu/NumberField.cs
+++ Assets/Game/Runtime/View/Menu/NumberField.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 namespace Game.Runtime.View.Menu
 {
-    [RequireComponent(typeof(TMP_InputField))]
     public class NumberField : Element, INumberField
     {
         [SerializeField] private TMP_InputField _origin;
